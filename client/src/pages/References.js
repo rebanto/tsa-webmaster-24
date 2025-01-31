f853a5c109d9a56998140b76a936b840b06975e6
Rebanto Nath <rebanto.nath@gmail.com>
2025-01-31 03:24:15 -0500

    work log and copyright form

--- client/src/pages/References.js
+++ client/src/pages/References.js
@@ -4,13 +4,21 @@ const References = () => {
   return (
     <div className="references-page" style={{ height: "calc(100vh - 410px)" }}>
       <h1>References:</h1>
-      <ul>
-        <li>
-          <a href="https://example.com" target="_blank">
-            Reference 1
-          </a>
-        </li>
-      </ul>
+      <p>TSA Copyright Checklist and Work Log</p>
+      <div style={{ display: "flex", justifyContent: "space-around", flexWrap: "wrap" }}>
+        <iframe 
+          src="/pdfs/Webmaster_Copyright_Form.pdf" 
+          width="45%" 
+          height="500px" 
+          title="Copyright Form"
+        />
+        <iframe 
+          src="/pdfs/Webmaster_Work_Log.pdf" 
+          width="45%" 
+          height="500px" 
+          title="Work Log"
+        />
+      </div>
     </div>
   );
 };
